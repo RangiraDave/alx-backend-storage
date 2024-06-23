82a927e322dd9cefa6b7ccaf1a4caf2d837f4e36
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:45:40 +0200

    Used $$ For delimiters

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -5,7 +5,7 @@
 -- The bands should be listed from the longest lifespan to the shortest.
 -- The output should be band_name and lifespan.
 
-SELECT band_name, (IFNULL(split, '2020') -formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(styles, '')) > 0
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
