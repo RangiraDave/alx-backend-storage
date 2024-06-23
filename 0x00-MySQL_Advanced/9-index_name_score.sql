bac4a55e96d1228ddd900cd9d00e906a35bc4035
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:48:15 +0200

    Only created the index

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,14 +2,4 @@
 -- Imports 'names.sql' dump.
 -- Indexes the first letter of the name and the first letter of the score.
 
--- Import the table dump
-cat names.sql | mysql -uroot -p holberton
-
--- Create the index
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), LEFT(score, 1));
-
--- Verify the index
-SHOW INDEX FROM names;
-
--- Perform a query using the index
-SELECT COUNT(name) FROM names WHERE name LIKE 'a%' AND score < 80;
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
