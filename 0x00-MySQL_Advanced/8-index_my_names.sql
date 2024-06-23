2bea49e5b7dda24e5ceeb5bdc88ae4850bf741b2
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:46:35 +0200

    Used $$ For delimiters

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@
 -- Only the first letter of the name is indexed.
 
 -- Create the index idx_name_first on the table names and the first letter of name
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1));
