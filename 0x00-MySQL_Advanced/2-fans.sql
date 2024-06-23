206b897b62b936814c84982832ec16aa6f597152
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:39:57 +0200

    Used one select

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,6 +3,6 @@
 
 -- Create a temporary table to store the aggregated results
 CREATE origin, SUM(fans) AS total_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY total_fans DESC;
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY total_fans DESC;
