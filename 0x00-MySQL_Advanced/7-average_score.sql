14f21a169fa91930997767276a374da652aa51e4
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:44:35 +0200

    Used $$ For delimiters

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,30 +1,24 @@
 -- Script to create 'corrections' table.
 -- Procedure 'ComputeAverageScoreForUser' that computes the average score for a user.
 
--- Create the stored procedure
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-    DECLARE average_score FLOAT;
-    
-    -- Compute the total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Compute the total number of projects for the user
-    SELECT COUNT(*) INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Compute the average score
-    SET average_score = total_score / total_projects;
-    
-    -- Update the average score for the user
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
     UPDATE users
-    SET average_score = average_score
-    WHERE id = user_id;
-END //
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
