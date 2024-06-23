0ccc74948f880b8e33cf08631828edb2c9bfc5e9
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:50:02 +0200

    Corrected typo

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 -- Imports 'names.sql' dump.
 -- Indexes the first letter of the name and the first letter of the score.
 
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
