66ff7e7e425ee2322f93d416b9a73d540502a8d3
RangiraDave <rangiradev666@gmail.com>
2024-06-23 09:37:26 +0200

    Used one select

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,12 +2,7 @@
 -- Assuming the table name is 'metal_bands'
 
 -- Create a temporary table to store the aggregated results
-CREATE TEMPORARY TABLE temp_band_fans AS
-SELECT origin, COUNT(*) AS nb_fans
+CREATE origin, SUM(fans) AS total_fans
 FROM metal_bands
-GROUP BY origin;
-
--- Rank the country origins of bands by the number of fans
-SELECT origin, nb_fans
-FROM temp_band_fans
-ORDER BY nb_fans DESC;
+GROUP BY origin
+ORDER BY total_fans DESC;
